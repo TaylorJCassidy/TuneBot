b3f179340c0208af9a7b127903841b94fa18f067
TaylorJCassidy <taylorjcassidy1@gmail.com>
2024-01-30 00:25:28 +0000

    stop cross channel joining and leaving

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -5,12 +5,8 @@ module.exports = {
     name: 'leave',
     aliases: ['getout', 'fuckoff'],
     run: function(msg, args, {guild}) {
-        const connection = getVoiceConnection(guild.guildID);
-        if (!connection) {
-            msg.channel.send(config.NOT_IN_CHANNEL);
-        }
-        else {
-            connection.disconnect();
-        }
+        const connection = getVoiceConnection(guild.guildId);
+        if (connection?.joinConfig.channelId == msg.member.voice.channel.id) connection.disconnect();
+        else msg.channel.send(config.NOT_IN_CHANNEL);
     }
 };
--- src/music/join.js
+++ src/music/join.js
@@ -1,13 +1,17 @@
 const {joinVoiceChannel, getVoiceConnection, entersState, VoiceConnectionStatus} = require('@discordjs/voice');
 const config = require('../configs/music.json');
-const createCustomAudioPlayer = require('./createCustomAudioPlayer');
 const logger = require('../utils/logger')('join');
 
 module.exports = (voiceChannel, guild) => {
     return new Promise((resolve, reject) => {
         if (!voiceChannel) return reject(new Error(config.NOT_IN_CHANNEL));
         if (!voiceChannel.joinable) return reject(new Error(config.UNABLE_TO_CONNECT));
-        if (getVoiceConnection(voiceChannel.guildId)) return resolve();
+
+        let connection = getVoiceConnection(voiceChannel.guildId);
+        if (connection) {
+            if (connection.joinConfig.channelId == voiceChannel.id) return resolve();
+            else return reject(new Error(config.ALREADY_IN_CHANNEL));
+        }
 
         const joinParams = {
             channelId: voiceChannel.id,
@@ -15,8 +19,8 @@ module.exports = (voiceChannel, guild) => {
             adapterCreator: voiceChannel.guild.voiceAdapterCreator
         };
 
-        const connection = joinVoiceChannel(joinParams);
-        entersState(connection, VoiceConnectionStatus.Ready, 3000).then(() => {
+        connection = joinVoiceChannel(joinParams);
+        entersState(connection, VoiceConnectionStatus.Ready, config.JOIN_TIMEOUT).then(() => {
             connection.subscribe(guild.audioPlayer);
 
             const disconnectListener = () => voiceChannel.members.size == 1 && connection.disconnect();
