b227a4ddf21c1ac9e663def86e68b5cf9a464761
TaylorJCassidy <taylorjcassidy1@gmail.com>
2024-01-30 01:54:22 +0000

    add validation to commands

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -1,12 +1,12 @@
 const {getVoiceConnection} = require('@discordjs/voice');
 const config = require('../configs/music.json');
+const valid = require('../music/valid');
 
 module.exports = {
     name: 'leave',
     aliases: ['getout', 'fuckoff'],
     run: function(msg, args, {guild}) {
-        const connection = getVoiceConnection(guild.guildId);
-        if (connection?.joinConfig.channelId == msg.member.voice.channel.id) connection.disconnect();
+        if (valid(msg, guild)) getVoiceConnection(guild.guildId).disconnect();
         else msg.channel.send(config.NOT_IN_CHANNEL);
     }
 };
--- src/commands/skip.js
+++ src/commands/skip.js
@@ -1,14 +1,18 @@
 const config = require('../configs/music.json');
+const valid = require('../music/valid');
 
 module.exports = {
     name: 'skip',
     aliases: ['s', 'fs'],
     run: function(msg, args, {guild}) {
-        if (guild.audioPlayer.skip()) {
-            msg.channel.send(config.SKIPPED);
+        if (valid(msg, guild)) {
+            if (guild.audioPlayer.skip()) {
+                msg.channel.send(config.SKIPPED);
+            }
+            else {
+                msg.channel.send(config.NO_SONG_TO_SKIP);
+            }
         }
-        else {
-            msg.channel.send(config.NO_SONG_TO_SKIP);
-        }
+        else msg.channel.send(config.NOT_IN_CHANNEL);
     }
 };
