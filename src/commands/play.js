d40077f3c64794361f1a6f39b49ddc74b762211c
TaylorJCassidy <taylorjcassidy1@gmail.com>
2024-02-24 15:29:02 +0000

    search for valid track before joining channel

--- src/commands/play.js
+++ src/commands/play.js
@@ -10,27 +10,29 @@ module.exports = {
     aliases: ['p'],
     run: function(msg, args, {guild}) {
         if (!args) return msg.channel.send(config.NO_TRACK_SEARCH_PROVIDED);
-        join(msg.member.voice.channel, msg.channel, guild)
-            .then(async () => {
-                getTrackInfo(args).then((track) => {
-                    if (!track) return msg.channel.send(config.UNABLE_TO_FIND_TRACK);
-                    const firstAdded = guild.audioPlayer.enqueue(track);
+        getTrackInfo(args)
+            .then((track) => {
+                if (!track) return msg.channel.send(config.UNABLE_TO_FIND_TRACK);
+                join(msg.member.voice.channel, msg.channel, guild)
+                    .then(() => {
+                        const firstAdded = guild.audioPlayer.enqueue(track);
 
-                    if (firstAdded) {
-                        msg.channel.send(config.PLAYING.replace('1', track.title));
-                    }
-                    else {
-                        msg.channel.send(config.QUEUED.replace('1', track.title));
-                    }
-                })
-                .catch((err) => {
-                    msg.channel.send(config.ERROR_GETTING_TRACK);
-                    logger(`Error getting video with search '${args}'\n${err}`, 'error');
-                });
+                        if (firstAdded) {
+                            msg.channel.send(config.PLAYING.replace('1', track.title));
+                        }
+                        else {
+                            msg.channel.send(config.QUEUED.replace('1', track.title));
+                        }
+                    })
+                    .catch((err) => {
+                        msg.channel.send(err.message);
+                    });
             })
             .catch((err) => {
-                msg.channel.send(err.message);
+                msg.channel.send(config.ERROR_GETTING_TRACK);
+                logger(`Error getting video with search '${args}'\n${err}`, 'error');
             });
+        
     }
 };
 
