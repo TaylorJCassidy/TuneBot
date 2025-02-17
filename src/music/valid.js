788056688d094f9a7da612670dd3f93fc3f6a827
TaylorJCassidy <taylorjcassidy1@gmail.com>
2025-02-17 21:28:29 +0000

    stop commands using guilds audio player before its ready, bump deps

--- src/music/valid.js
+++ src/music/valid.js
@@ -2,6 +2,6 @@ const {getVoiceConnection} = require('@discordjs/voice');
 
 module.exports = (msg, guild) => {
     const connection = getVoiceConnection(guild.guildId);
-    if (!connection || !msg.member.voice.channel) return false;
+    if (!connection || !msg.member.voice.channel || !guild.audioPlayer) return false;
     return connection.joinConfig.channelId == msg.member.voice.channel.id;
 };
